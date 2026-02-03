3b158a274a1c0e3b056011f1205f9fcfabaa276c
Pierre Kerlau <pierre.kerlau@viacesi.fr>
2026-02-03 15:25:11 +0100

    Fix spagetti code into splitted code

---
+++ include/security_audio.h
@@ -0,0 +1,9 @@
+#ifndef SECURITY_AUDIO_H
+#define SECURITY_AUDIO_H
+
+void playPressBeep(int buzzerPin);
+void playGoodCombinationSound(int buzzerPin);
+void playWrongCombinationSound(int buzzerPin);
+
+#endif
+
